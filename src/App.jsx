c20e92ea70c83e5ba49fd9991dbbed3c9799f2a6
lesnikovskiy <lesnikovski@gmail.com>
2025-04-29 22:27:55 +0300

    Detect draw result in a game

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Log from './components/Log';
 import GameBoard from './components/Gameboard';
+import GameOver from './components/GameOver.jsx';
 import Player from './components/Player';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
@@ -53,6 +54,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -73,7 +76,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
       </div>
       <Log turns={gameTurns} />
