9fc7386b9f986ed60eaee8754f9af964529474b2
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2025-04-29 17:44:34 +0300

    Reduce State Management

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,27 @@ import Log from './components/Log';
 import GameBoard from './components/Gameboard';
 import Player from './components/Player';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currActive) => (currActive === 'X' ? 'O' : 'X'));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { player: activePlayer, square: { row: rowIndex, col: colIndex } },
+        { player: currentPlayer, square: { row: rowIndex, col: colIndex } },
         ...prevTurns,
       ];
 
